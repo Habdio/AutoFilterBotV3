fff38fadb296159aba4189509ba4c118adca14d0
Habdio <87224211+Habdio@users.noreply.github.com>
2022-02-24 07:19:02 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,13 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Cyber Dark ]
+
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ For Deploy : [Join Channel] https://t.me/+o0magU0LHq5jZmQ9
+
 """
